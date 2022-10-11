39d391f6e00366836835dee80fdc02a7d0a5c978
adtyarizkii <aditya.rizki0501@gmail.com>
2022-10-11 07:19:27 +0700

    Test Mentor

---
+++ Javascript/README.md
@@ -0,0 +1,12 @@
+## Simple Function
+
+> File : `index.js`
+
+Inside file index.js create a simple function :
+
+```javascript
+function test() {
+  alert("Test mentor DW");
+}
+test();
+```
---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+function test() {
+  alert("Test mentor DW");
+}
+test();
+
+let num1 = 1;
+let num2 = 2;
+
+function add(one, two) {
+  return one + two;
+}
+
+console.log(add(num1, num2));
